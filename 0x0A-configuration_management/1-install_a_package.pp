df17fd2bd6d679dfff6a85a7b2b0d974c86f920c
jasmineH-D <mellyasminita2000@gmail.com>
2023-09-22 15:17:15 -0700

    tasks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# install puppet-lint using Puppet
+# installs flask from pip3
 
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
