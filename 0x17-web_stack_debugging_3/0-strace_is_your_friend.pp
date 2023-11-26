7d276001930b9c7cc4278d2af4ef096d9ced541b
yasmine haddad <122939862+jasmineH-D@users.noreply.github.com>
2023-11-26 15:35:19 +0100

    tasks

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# fix exntetion phpp to php
+#fix 500 return apache using puppet
 
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path => '/usr/local/bin/:/bin/'
+  path    => '/usr/local/bin/:/bin/'
 }
